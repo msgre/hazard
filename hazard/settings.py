075fd4c2d0d05a26cef4abb730c321c905a07dcc
Michal Valoušek <michal@plovarna.cz>
2011-05-08 07:13:43 +0200

    Potuneni settings

--- hazard/settings.py
+++ hazard/settings.py
@@ -19,11 +19,11 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Prague'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs-CZ'
 
 SITE_ID = 1
 
@@ -74,29 +74,21 @@ INSTALLED_APPS = (
     'hazard.geo',
 )
 
+APPEND_SLASH = True
+
+# datumy
+DATE_FORMAT = "j. F Y"
+TIME_FORMAT = "G.i"
+
+# debug
+SEND_BROKEN_LINK_EMAILS = True
+EMAIL_SUBJECT_PREFIX = u'[MapyHazardu]'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request':{
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
