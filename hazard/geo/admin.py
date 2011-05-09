d85a66f1d4f3fbeef2a3c9bf2828fb8f8c2e2269
Michal Valoušek <michal@plovarna.cz>
2011-05-09 22:15:03 +0200

    Pohodlnejsi sprava zaznamu v administraci

--- hazard/geo/admin.py
+++ hazard/geo/admin.py
@@ -3,7 +3,35 @@
 from django.contrib import admin
 from hazard.geo.models import Entry, Zone, Building, Hell
 
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'slug', 'public', 'dperc', 'dhell_count', 'dper_population', 'dper_area' )
+    list_filter = ('public', )
+    list_editable = ('public', 'slug', )
+    readonly_fields = ('wikipedia', 'hell_url', 'building_url', 'ip')
+    date_hierarchy = 'created'
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'description', 'email', 'public')
+        }),
+        (u'Informace vysosnuté z Wikipedie', {
+            'fields': ('population', 'area', 'wikipedia')
+        }),
+        (u'Doplňující informace', {
+            'classes': ('collapse',),
+            'fields': ('hell_url', 'building_url', 'ip')
+        }),
+    )
+
+
+    def save_model(self, request, obj, form, change):
+        """
+        Pri ulozeni objektu prepocitame
+        """
+        obj.recalculate_denormalized_values(counts=True)
+        obj.save()
+
+admin.site.register(Entry, EntryAdmin)
 admin.site.register(Zone)
 admin.site.register(Building)
 admin.site.register(Hell)
