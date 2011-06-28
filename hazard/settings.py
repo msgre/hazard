ec6a659553ee8fb3f74cd634224ca274926dcee1
Michal Valoušek <michal@plovarna.cz>
2011-06-28 13:06:04 +0200

    Trosku nam to zlobi

--- hazard/settings.py
+++ hazard/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE_CLASSES = (
     'hazard.geo.middleware.ProcessingLimitMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
