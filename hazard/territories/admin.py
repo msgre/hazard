1af9f4d67008c78d1835cb3a5fcb4c82a6e42ab9
Michal Valoušek <michal@plovarna.cz>
2012-01-04 07:51:15 +0100

    Slucovani obci v administraci

--- hazard/territories/admin.py
+++ hazard/territories/admin.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.conf.urls.defaults import patterns, url
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.utils.safestring import mark_safe
 
 from hazard.territories.models import Region, District, Town, Zip
+from hazard.territories.forms import MergeTownAdminForm
 
 
 class DistrictAdmin(admin.ModelAdmin):
@@ -12,13 +19,74 @@ class DistrictAdmin(admin.ModelAdmin):
 
 
 class TownAdmin(admin.ModelAdmin):
-    list_display = ('title', 'district', 'region', 'display_zip')
+    list_display = ('title', 'district', 'region', 'display_zip', 'population')
     search_fields = ('title', 'slug')
     list_filter = ('region', 'district', )
+    actions = ['merge_action']
 
     def display_zip(self, obj):
         return u", ".join([i.display() for i in obj.zip_set.all()])
     display_zip.short_description = u"PSČ"
+
+    def get_urls(self):
+        urls = super(TownAdmin, self).get_urls()
+        my_urls = patterns('',
+            url(r'^merge/$', self.admin_site.admin_view(self.merge_view), name="territories-town-merge")
+        )
+        return my_urls + urls
+
+    def merge_action(self, request, queryset):
+        """
+        Akce do administrace -- slouceni obci. Dojde zde pouze k redirectu
+        na custom admin view.
+        """
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        url = reverse('admin:territories-town-merge')
+        return HttpResponseRedirect("%s?ids=%s" % (url, ",".join(selected)))
+    merge_action.short_description = u"Sloučit vybrané obce"
+
+    def merge_view(self, request):
+        """
+        TODO:
+        """
+        # vyzobneme IDcka z URL
+        ids = [int(i) for i in request.GET.get('ids', '').split(',') if i.isdigit()]
+        if not ids:
+            raise Http404
+        queryset = Town.objects.filter(id__in=ids)
+
+        # obsluha formiku
+        if request.method == 'POST':
+            form = MergeTownAdminForm(queryset, request.POST)
+            if form.is_valid():
+                log = form.save()
+                msg = u", ".join([u"%s=%i" % (k, log[k]) for k in log])
+                self.message_user(request, u"Sloučení obce úspěšně provedeno (%s)" % msg)
+                return HttpResponseRedirect(reverse('admin:territories_town_changelist'))
+        else:
+            form = MergeTownAdminForm(queryset)
+
+        # potrava pro sablonu
+        adminForm = admin.helpers.AdminForm(form, [(None, {
+            'fields': ('towns', ),
+            'description': u'<p>Vyber obec, pod kterou se ostatní přesunou.</p>'
+        })], {})
+        media = self.media + adminForm.media
+
+        # sup do sablony
+        context = {
+            'is_popup': False,
+            'app_label': self.model._meta.app_label,
+            'verbose_name': self.model._meta.verbose_name,
+            'title': u'Sloučení obcí',
+            'adminform': adminForm,
+            'media': mark_safe(media),
+            'original': None # TODO: muze to tak byt?
+        }
+        self.change_form_template = 'admin/territories/town/merge.html'
+        return self.render_change_form(request, context, change=True)
+
+
 
 
 class ZipAdmin(admin.ModelAdmin):
---
+++ hazard/territories/forms.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# vim: set et si ts=4 sw=4:
+
+from django import forms
+from hazard.shared.models import repair_referencies
+
+
+class MergeTownAdminForm(forms.Form):
+    """
+    Formik pro custom admin view, ve kterem se voli obec, pod kterou se
+    slouci vsechny ostatni vybrane.
+    """
+    towns = forms.ModelChoiceField(label=u'Obce', queryset=None, empty_label=None)
+
+    def __init__(self, queryset, *args, **kwargs):
+        super(MergeTownAdminForm, self).__init__(*args, **kwargs)
+        self.fields['towns'].queryset = queryset
+        choices = list(self.fields['towns'].choices)
+        if choices:
+            self.fields['towns'].initial = choices[0][0]
+        self.fields['towns'].widget = forms.RadioSelect(choices=choices)
+
+    def save(self):
+        """
+        TODO:
+        """
+        chief = self.cleaned_data['towns']
+        others = self.fields['towns'].queryset.exclude(id=chief.id)
+        out = {}
+        for other in others:
+            log = repair_referencies(other, chief)
+            other.delete()
+            for k in log['affected']:
+                if k not in out:
+                    out[k] = 0
+                out[k] += len(log['affected'][k])
+        return out
