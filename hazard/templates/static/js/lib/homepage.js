91dd5cc133240a7d48c353e15ef7d176b2b4b840
Michal Valoušek <michal@plovarna.cz>
2012-03-08 16:16:15 +0100

    Vyskakovaci okno na Homepage, upozornujici na puvodni verzi MH

--- hazard/templates/static/js/lib/homepage.js
+++ hazard/templates/static/js/lib/homepage.js
@@ -1,7 +1,8 @@
 (function() {
   /*
   Naseptavac na uvodni strance.
-  */  var renderMenu;
+  */  var COOKIE_NAME, oldMHModal, renderMenu;
+  COOKIE_NAME = 'dialog_opened';
   renderMenu = function(ul, items) {
     var current_category, self;
     self = this;
@@ -14,16 +15,38 @@
       return self._renderItem(ul, item);
     });
   };
+  oldMHModal = function() {
+    var html;
+    if (!$.cookie(COOKIE_NAME)) {
+      html = "<div id=\"dialog\" title=\"Upozornění\" style=\"display:none\">\n    <p><strong>MapyHazardu se mění!</strong></p>\n\n    <p>V březnu 2012 jsme naše stránky zásadně změnili. Chceme o hazardu\n    informovat v širších souvislostech a tento přerod nám to umožní.</p>\n\n    <p>Původní aplikaci, s pomocí které může kdokoliv prověřovat\n    konflikty heren poblíž dříve chráněných budov, jsme ale ponechali.\n    Najdete ji na adrese\n    <a href=\"http://puvodni.mapyhazardu.cz/\">puvodni.mapyhazardu.cz</a>.\n</div>";
+      $('body').append(html);
+      $('#dialog').dialog({
+        width: 500,
+        modal: true,
+        resizable: false,
+        draggable: false,
+        buttons: {
+          'Rozumím': function() {
+            return $(this).dialog('close');
+          }
+        }
+      });
+      return $.cookie(COOKIE_NAME, '1', {
+        expires: 365
+      });
+    }
+  };
   $(document).ready(function() {
     $.widget('custom.catcomplete', $.ui.autocomplete, {
       _renderMenu: renderMenu
     });
-    return $('#search').catcomplete({
+    $('#search').catcomplete({
       delay: 0,
       source: '/autocomplete/',
       select: function(event, ui) {
         return window.location = ui.item.url;
       }
     });
+    return oldMHModal();
   });
 }).call(this);
--- hazard/templates/static/js/src/homepage.coffee
+++ hazard/templates/static/js/src/homepage.coffee
@@ -2,6 +2,8 @@
 Naseptavac na uvodni strance.
 ###
 
+COOKIE_NAME = 'dialog_opened'
+
 renderMenu = (ul, items) ->
     self = @
     current_category = ''
@@ -10,6 +12,32 @@ renderMenu = (ul, items) ->
             ul.append("<li class='ui-autocomplete-category'>#{ item.category }</li>")
             current_category = item.category
         self._renderItem(ul, item)
+
+oldMHModal = () ->
+    if not $.cookie(COOKIE_NAME)
+        html = """
+            <div id="dialog" title="Upozornění" style="display:none">
+                <p><strong>MapyHazardu se mění!</strong></p>
+
+                <p>V březnu 2012 jsme naše stránky zásadně změnili. Chceme o hazardu
+                informovat v širších souvislostech a tento přerod nám to umožní.</p>
+
+                <p>Původní aplikaci, s pomocí které může kdokoliv prověřovat
+                konflikty heren poblíž dříve chráněných budov, jsme ale ponechali.
+                Najdete ji na adrese
+                <a href="http://puvodni.mapyhazardu.cz/">puvodni.mapyhazardu.cz</a>.
+            </div>
+        """
+        $('body').append(html)
+        $('#dialog').dialog
+            width: 500
+            modal: true
+            resizable: false
+            draggable: false
+            buttons:
+                'Rozumím': () -> $(@).dialog('close')
+        $.cookie(COOKIE_NAME, '1', {expires: 365})
+
 
 $(document).ready () ->
     $.widget('custom.catcomplete', $.ui.autocomplete, {
@@ -21,3 +49,6 @@ $(document).ready () ->
         source: '/autocomplete/'
         select: (event, ui) ->
             window.location = ui.item.url
+
+    oldMHModal()
+
